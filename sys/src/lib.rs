bdeb585b0c8aa952dae34ed1ddddd064218ef6f8
James Cape <jamescape777@gmail.com>
2025-01-14 03:41:53 +0000

    Fix warnings, pre-gen the bindings rather than build.rs

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -1,7 +1,9 @@
-/// Unofficial Rust FFI for XCAPI
+//! Unofficial Rust FFI for XCAPI
 
+/// The generated code will go into gen.
+#[allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 mod gen {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+    include!("gen.inc.rs");
 }
 
 pub use gen::*;
