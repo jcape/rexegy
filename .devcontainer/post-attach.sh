36c32a1a30e33cf93265caecb8cfe203b197c0a1
James Cape <jamescape777@gmail.com>
2025-02-25 05:56:53 +0000

    build: fix cargo install of binstall

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm install -g @withgraphite/graphite-cli@stable
-cargo install -y cargo-binstall
+cargo install cargo-binstall
 cargo binstall -y release-plz
 cargo binstall -y cargo-semver-checks
 
