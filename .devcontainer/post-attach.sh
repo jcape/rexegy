5ca35f6e22b3814e105f30eb311bdcb29bcdce79
James Cape <jcape@clearstreet.io>
2025-02-28 20:37:19 +0000

    build: use local cargo/pre-commit cache.

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -4,6 +4,10 @@ mkdir -p /workspaces/rxegy/.cache/cargo
 mkdir -p /workspaces/rxegy/.cache/pre-commit
 export CARGO_HOME=/workspaces/rxegy/.cache/cargo
 
+cargo install -q cargo-binstall
+cargo install -q cargo-semver-checks
+cargo binstall -y release-plz
+
 pushd /workspaces/rxegy >/dev/null
 pre-commit install >/dev/null
 popd >/dev/null
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 npm install -g @withgraphite/graphite-cli@stable
-cargo install -q cargo-binstall
-cargo install -q cargo-semver-checks
-cargo binstall -y release-plz
