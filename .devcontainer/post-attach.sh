b1b951b8d5b3b4c6326ba01fd79274332005f422
James Cape <jamescape777@gmail.com>
2025-03-03 09:31:36 +0000

    fix: post-attach install cargo tools to the path.

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 mkdir -p /workspaces/rxegy/.cache/cargo
-mkdir -p /workspaces/rxegy/.cache/pre-commit
+if [ -d "/workspaces/rxegy/.cache/cargo/bin" ]; then
+    rm -rf /workspaces/rxegy/.cache/cargo/bin
+fi
+ln -sf /usr/local/cargo/bin /workspaces/rxegy/.cache/cargo/
 export CARGO_HOME=/workspaces/rxegy/.cache/cargo
 
 cargo install -q cargo-binstall
 cargo install -q cargo-semver-checks
 cargo binstall -y release-plz
+
+mkdir -p /workspaces/rxegy/.cache/pre-commit
+export PATH=$PATH:/workspaces/rxegy/.cache/cargo/bin
 
 pushd /workspaces/rxegy >/dev/null
 pre-commit install >/dev/null
